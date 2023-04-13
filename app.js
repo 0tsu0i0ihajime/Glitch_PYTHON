9f983325f5e0b8b1fb2c2f4b9a95d14d9c0ea5f6
0tsu0i0ihajime <110276910+0tsu0i0ihajime@users.noreply.github.com>
2023-04-13 13:55:50 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const ytdl = require("ytdl-core");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+let qrCodeRead = false;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "QRREADER.html"));
+});
+
+app.post("/data", (req, res) => {
+  const qrCodeData = req.body.qr;
+  //console.log(`QRコードから読み込まれたデータ：${qrCodeData}`);
+  const validQRCodeData = "04afahfakjfvizovhsoigeewpUwr326";
+  if (qrCodeData === validQRCodeData) {
+    //const expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000); //1年間有効なCookieを設定
+    //res.cookie("authToken", "validToken", { expires: expires });
+    res.redirect('/stream.html')
+  } else {
+    res.status(400).send('Invalid QR Code');
+  }
+});
+
+
+app.get("/stream", async (req, res) => {
+  const videoUrl = req.query.videoUrl;
+  const video = ytdl(videoUrl, {
+    quality:'highestaudio',
+    filter:'audioonly'
+  });
+  //const audioStream = ytdl(videoUrl, { filter: "audioonly" });
+  const filepath = path.join(__dirname, 'temp', `${Date.now()}.mp3`);
+  const filestream = fs.createWriteStream(filepath);
+  video.pipe(filestream);
+  res.set({
+    'Content-Type':'audio/mpeg',
+    'Transfer-Encoding':'chunked'
+  });
+  video.pipe(res);
+  video.on('end', ()=>{
+    fs.unlink(filepath, (err)=>{
+      if(err) console.log(err);
+    });
+  });
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on port ${process.env.PORT || 8080}`);
+});
